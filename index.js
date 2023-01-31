42efcb7f808a99d2e1c6e16fbda5d51b7015c3cc
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-01-31 10:39:51 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() + 1);
+
+let num3 = 12;
+let num4 = 3;
+const mod = num3 / num4;
+
+const max = Math.max(20, 17, 12, 8, 4);
